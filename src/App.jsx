11296a112f8867956244dba97f08f42ede470d87
Javier Veron <javier.veron@gmail.com>
2023-02-13 21:09:36 -0300

    After Class 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import Destacado from "./components/Destacado";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
 import NavBar from "./components/NavBar";
 import PromoApp from "./components/PromoApp";
+import Error404 from "./components/Error404";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <ItemListContainer greeting={"No se encontraron Productos!"} />
-      <Destacado />
-      <PromoApp />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path={"/"} element={<ItemListContainer />} />
+          <Route path={"/category/:id"} element={<ItemListContainer />} />
+          <Route path={"/item/:id"} element={<ItemDetailContainer />} />
+          <Route path={"*"} element={<Error404 />} />
+        </Routes>
+        <PromoApp />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
